db57a030f7aba2476d451bb20c428158653ea881
akili <amwawe@gmail.com>
2022-01-20 13:22:06 -0600

    additions

--- public/js/app.js
+++ public/js/app.js
@@ -43,3 +43,12 @@ weatherForm.addEventListener('submit', (event) => {
         }) 
     });
 })
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
